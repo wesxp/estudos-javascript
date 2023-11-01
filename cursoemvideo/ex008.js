af08e498605b7fb1a7c8d0f8e7ea1801f526754b
wesley oliveira <116278696+wesxp@users.noreply.github.com>
2023-11-01 10:55:24 -0300

    colocando em pratica  o assunto sobre condições em JS

---
+++ cursoemvideo/ex008.js
@@ -0,0 +1,7 @@
+var vel=70
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel>60.5){
+    console.log('Você ultrapassou a velocidade máxima ,multado(a)')
+}
+console.log('======================================')
+console.log('Diriga sempre com cinto de segurança!!')
